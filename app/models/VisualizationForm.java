f6f8d3de8869c7b9201430ac06e8cc904e40a8af
limor ahuvi <limorahuvi@gmail.com>
2019-03-11 07:43:47 +0200

    coors handling

--- app/models/VisualizationForm.java
+++ app/models/VisualizationForm.java
@@ -3,13 +3,23 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 
 public abstract class VisualizationForm{
+    // Result
     public JsonNode result = null;
     public String resultString = "";
-    public String coor1;
-    public String coor2;
-//TODO get&set coors
 
+    // Coordinates
+    public final int NumberOfPointsNeeded = 2;
+    public String[] points = null;
+    private int number_of_points_inserted;
+
+    //    public String coor1;
+    //    public String coor2;
+    //TODO get&set coors
+
+    //  abstract
     public abstract String getParametersList();
+
+    // result
     public JsonNode getResult(){
         return result;
     }
@@ -20,6 +30,36 @@ public abstract class VisualizationForm{
 
     public String getResultString(){
         return resultString;
+    }
+
+    // coordinates
+    public int getNumberOfPointsNeeded(){return NumberOfPointsNeeded;};
+
+    public String[] getPoints(){return points;}
+
+    public boolean addPoint(String point){
+        if(points==null){
+            points = new String[NumberOfPointsNeeded];
+        }
+        if(number_of_points_inserted>=NumberOfPointsNeeded){
+            return false;
+        }
+        points[number_of_points_inserted++] = point;
+        return true;
+    }
+
+    public boolean removePoint(int index){
+        points[index]=points[--number_of_points_inserted];
+        return true;
+    }
+
+    public boolean removePoint(String coor){
+        for (int i=0; i<number_of_points_inserted; i++) {
+            if(points[i].equals(coor)){
+                return removePoint(i);
+            }
+        }
+        return false;
     }
 }
 
