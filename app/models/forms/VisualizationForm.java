f35c95439040e7cad7d8d719f104f37a9925eab9
limor ahuvi <limorahuvi@gmail.com>
2019-03-12 14:39:21 +0200

    added coordinates to form

--- app/models/forms/VisualizationForm.java
+++ app/models/forms/VisualizationForm.java
@@ -17,9 +17,11 @@ public abstract class VisualizationForm{
     public String resultString = "";
 
     // Coordinates
-    public final int NumberOfPointsNeeded = 2;
-    public String[] points = null;
-    private int number_of_points_inserted;
+//    public final int NumberOfPointsNeeded = 2;
+//    public Object[] points = null;
+//    private int number_of_points_inserted;
+    public String coor1="";
+    public String coor2="";
 
 
     //  abstract
@@ -64,33 +66,43 @@ public abstract class VisualizationForm{
     }
 
     // coordinates
-    public int getNumberOfPointsNeeded(){return NumberOfPointsNeeded;};
+    public String getCoor1(){return coor1;}
 
-    public String[] getPoints(){return points;}
+    public void setCoor1(String coor){this.coor1=coor;}
 
-    public boolean addPoint(String point){
-        if(points==null){
-            points = new String[NumberOfPointsNeeded];
-        }
-        if(number_of_points_inserted>=NumberOfPointsNeeded){
-            return false;
-        }
-        points[number_of_points_inserted++] = point;
-        return true;
-    }
+    public String getCoor2(){return coor2;}
 
-    public boolean removePoint(int index){
-        points[index]=points[--number_of_points_inserted];
-        return true;
-    }
+    public void setCoor2(String coor){this.coor2=coor;}
 
-    public boolean removePoint(String coor){
-        for (int i=0; i<number_of_points_inserted; i++) {
-            if(points[i].equals(coor)){
-                return removePoint(i);
-            }
-        }
-        return false;
-    }
+//    public int getNumberOfPointsNeeded(){return NumberOfPointsNeeded;};
+//
+//    public Object[] getPoints(){return points;}
+//
+//    public void setPoints(Object[] points){this.points=points;}
+
+//    public boolean addPoint(String point){
+//        if(points==null){
+//            points = new String[NumberOfPointsNeeded];
+//        }
+//        if(number_of_points_inserted>=NumberOfPointsNeeded){
+//            return false;
+//        }
+//        points[number_of_points_inserted++] = point;
+//        return true;
+//    }
+//
+//    public boolean removePoint(int index){
+//        points[index]=points[--number_of_points_inserted];
+//        return true;
+//    }
+//
+//    public boolean removePoint(String coor){
+//        for (int i=0; i<number_of_points_inserted; i++) {
+//            if(points[i].equals(coor)){
+//                return removePoint(i);
+//            }
+//        }
+//        return false;
+//    }
 }
 
