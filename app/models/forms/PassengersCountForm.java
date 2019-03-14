b386d74adc60aadb13584357ac8b860570772573
coral <shaharcoral@gmail.com>
2019-03-14 20:25:48 +0200

    done todo-print without hour in date

--- app/models/forms/PassengersCountForm.java
+++ app/models/forms/PassengersCountForm.java
@@ -3,6 +3,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import models.VisualizationForm;
 import play.data.validation.Constraints;
 import play.data.validation.Constraints.Validate;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PassengersCountForm extends VisualizationForm{
 
@@ -21,14 +23,27 @@ public class PassengersCountForm extends VisualizationForm{
         this.minPassengersForPublicLane=minPassengersForPublicLane;
     }
 
-//    TODO print dates without time
     public String getParametersList(){
-        return "Start Date: "+startDate+", "+
-                "End Date: "+endDate+", "+
-                "Day: "+day+", "+
-                "Start Hour: "+startHour+", "+
-                "End Hour: "+endHour+", "+
-                "Minimum Passengers For Public Lane: "+minPassengersForPublicLane;
+        String strDate="";
+        String strDate2="";
+        if(startDate!=null) {
+            Date date = startDate;
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            strDate = formatter.format(date);
+        }
+        if(endDate!=null) {
+            Date date2 = endDate;
+            SimpleDateFormat formatter2 = new SimpleDateFormat("dd/MM/yyyy");
+            strDate2= formatter2.format(date2);
+        }
+
+        return "Start Date: "+strDate+", "+
+                    "End Date: "+strDate2+", "+
+                    "Day: "+day+", "+
+                    "Start Hour: "+startHour+", "+
+                    "End Hour: "+endHour+", "+
+                    "Minimum Passengers For Public Lane: "+minPassengersForPublicLane;
+
     }
 
 
