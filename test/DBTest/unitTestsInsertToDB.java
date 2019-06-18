f46d0dda127888faf3fd684fc872483b498f8eb8
Hadas Shidlovski <shidlhad@post.bgu.ac.il>
2019-06-18 20:59:30 +0300

    update testunzip after refactor insertToDB

--- test/DBTest/unitTestsInsertToDB.java
+++ test/DBTest/unitTestsInsertToDB.java
@@ -2,9 +2,11 @@ package DBTest;
 
 import models.entities.Calendar;
 import models.*;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
@@ -16,11 +18,21 @@ public class unitTestsInsertToDB {
     String destDir = utilitiesDBtest.createPath("outTest");
 
     @BeforeClass
-    public void setup() throws IOException {
+    public static void setup() throws IOException {
         utilitiesFunc.writeToLog("LogTest_unitTestsInsertToDB.log");
+        utilitiesFunc.logger.info("unitTests Unser To DB TEST starts: \n");
     }
+
+    @AfterClass
+    public static void teardown() throws IOException {
+        utilitiesFunc.logger.info("tablesQueries test ends... \n");
+    }
+
     @Test
     public void testUnzip() throws Exception {
+        File dir = new File(destDir);
+        if(!dir.exists())
+            dir.mkdirs();
         utilitiesFunc.unzip(destDir);
         assertTrue(!destDir.isEmpty());
     }
@@ -30,12 +42,11 @@ public class unitTestsInsertToDB {
         /*test to check if time for unzipping file is less than 2 min*/
         //check start time for unzipping
         Date startDate =  new Date();
-        utilitiesFunc.logger.info("tablesQueries TEST starts: \n");
-        utilitiesFunc.logger.info("Start time: "+ startDate);
+        utilitiesFunc.logger.info("Start time for unzipping: "+ startDate);
         //System.out.println("Start time:"+ startDate);
         utilitiesFunc.unzip(destDir);
         Date endDate =  new Date();
-        utilitiesFunc.logger.info("End time: "+ endDate);
+        utilitiesFunc.logger.info("End time for unzipping: "+ endDate);
         //System.out.println("End time:"+ endDate);
         utilitiesDBtest.deleteOutputTest(destDir);
         //assertEquals(true, startDate.before(endDate));
